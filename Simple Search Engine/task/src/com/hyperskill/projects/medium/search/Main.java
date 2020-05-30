1ff8f87bbdf8848d3d1de100edfd1cd9b9315e8d
Nick <mimoun136@yandex.ru>
2020-05-30 16:56:41 +0900

    Initial commit

---
+++ Simple Search Engine/task/src/com/hyperskill/projects/medium/search/Main.java
@@ -0,0 +1,12 @@
+package com.hyperskill.projects.medium.search;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.nextLine();
+        String[] storeInformation = temp.split(" ");
+        String searchInformation = scanner.nextLine();
+    }
+}
